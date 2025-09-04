228cfae66d6433dfa60bebac19f8782713e491e3
Alexander <02481632@proton.me>
2025-09-04 17:32:11 +0300

    feat(preview): add StoryGrid template preview with photo mapping

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useAutoThemeClass } from './hooks/useAutoThemeClass';
 import TextSection from './components/TextSection';
 import UploadPanel from "@/components/UploadPanel";
 import TemplatePicker from "@/components/TemplatePicker";
+import StoryGrid from "@/components/StoryGrid";
 
 export default function App() {
   useAutoThemeClass();
@@ -16,6 +17,7 @@ export default function App() {
       <main className="mx-auto max-w-screen-md px-4 space-y-8">
         <UploadPanel />
         <TemplatePicker />
+        <StoryGrid />
         <TextSection />
       </main>
     </>
